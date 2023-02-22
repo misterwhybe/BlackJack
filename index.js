a8b45c0070b4df060dadb07c9e39318f06362805
misterwhybe <wiebejelsma@hotmail.com>
2023-02-22 18:13:45 +0100

    New updates

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.querySelector(".sum-el")
+let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+
+    let temp = Math.floor( Math.random()*13 ) + 1
+    if(temp == 1){
+        temp = 11
+    }
+    else if(temp >= 11){
+        temp = 10
+    }
+    return temp
+}
+
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -19,6 +56,15 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
-    sumEl.textContent = 'Sum: ' + sum
-    cardsEl.textContent = 'Cards: ' + firstCard + " " + secondCard
+}
+
+
+function newCard() {
+    if(isAlive && !hasBlackJack){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
 }
